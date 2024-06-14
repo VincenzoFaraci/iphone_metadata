b35c4749be8c20a86776018152438cfe317b91d1
VincenzoFaraci <85934639+VincenzoFaraci@users.noreply.github.com>
2024-06-14 11:04:06 +0200

    Update delete_exif.py

--- src/utils/delete_exif.py
+++ src/utils/delete_exif.py
@@ -34,9 +34,18 @@ def remove_exif(image_path):
         json.dump(image_exif, f, indent=4)
 
 
-def remove_multiple_exif(image_folder):
-    for filename in os.listdir(image_folder):
+def remove_multiple_exif(image_folder_path):
+    """
+    Remove EXIF data for all the provided image file in the folder.
+    
+    Args:
+        image_folder_path (str): The path to the image file.
+        
+    Returns:
+        None
+    """
+    for filename in os.listdir(image_folder_path):
         if filename.lower().endswith(('.jpg', '.jpeg', '.png', '.tiff')):
-            image_path = os.path.join(image_folder, filename)
+            image_path = os.path.join(image_folder_path, filename)
             with ExifTool() as e:
                 e.execute(f'-all=', image_path.encode())
